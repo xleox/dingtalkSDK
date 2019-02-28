4640c18737a739794f8f9c16d2ec555aa2090637
loyehu <2690264337@qq.com>
2019-02-28 16:06:19 +0800

    update

--- api/Rente.js
+++ api/Rente.js
@@ -14,7 +14,6 @@ var addSendMessageMission = function(messageData){
     if (messageData.type === 'text') {
         temp.msgtype = 'text';
         temp.text = {
-            "title": messageData.title,
             "content": messageData.content
         };
     } else {
