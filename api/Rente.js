86af03a33149a3163eb5080cbfa3ccd91a680032
loyehu <2690264337@qq.com>
2019-02-28 15:55:39 +0800

    update

--- api/Rente.js
+++ api/Rente.js
@@ -68,7 +68,7 @@ router.post('/sendMessage',(req,res)=>{
     }
     var messageData = {
         type: req.body.type,
-        title: req.body.title,
+        // title: req.body.title,
         userID: req.body.userID,
     };
     if (req.body.type !== 'text') {
@@ -77,102 +77,6 @@ router.post('/sendMessage',(req,res)=>{
         messageData.content = req.body.content;
     }
     addSendMessageMission(messageData);
-
-    // if (req.body.messageUrl === undefined || req.body.picUrl === undefined || req.body.messagePrincipal === undefined || req.body.messageName === undefined || req.body.messageOrderNumber === undefined || req.body.messageShopName === undefined || req.body.messageShopSku === undefined || req.body.messageShopPrice === undefined || req.body.messageSaleDitch === undefined || req.body.senduserId === undefined) {
-    //     res.send('格式错误');
-    //     return;
-    // }
-    // if (req.body.messageUrl === '' || req.body.picUrl === '' || req.body.messagePrincipal === '' || req.body.messageName === '' || req.body.messageOrderNumber === '' || req.body.messageShopName === '' || req.body.messageShopSku === '' || req.body.messageShopPrice === '' || req.body.messageSaleDitch === '' || req.body.senduserId === '') {
-    //     res.send('格式错误');
-    //     return;
-    // }
-    // var message = {
-    //     messageUrl: req.body.messageUrl,
-    //     picUrl: req.body.picUrl,
-    //     messagePrincipal: req.body.messagePrincipal,
-    //     messageName: req.body.messageName,
-    //     messageOrderNumber: req.body.messageOrderNumber,
-    //     messageShopName: req.body.messageShopName,
-    //     messageShopSku: req.body.messageShopSku,
-    //     messageShopPrice: req.body.messageShopPrice,
-    //     messageSaleDitch: req.body.messageSaleDitch,
-    //     senduserId: req.body.senduserId,
-    // };
-    // addSendMessageMission(message);
 });
-
-
-// 发送文本消息
-// var addSendTextMessageMission = function(messageData){
-//     dingtalk.message.send({
-//         touser: messageData.userId,
-//         agentid: "210810582",
-//         msgtype: "link",
-//         link: {
-//             "title": messageData.textTitle,
-//             "text": messageData.textContent
-//          }
-//     }).then(msg=>{
-//         console.log('消息发送', msg);
-//         dingtalk.message.listMessageStatus(msg.messageId)
-//             .then(doc=>{
-//                 console.log("消息状态", doc);
-//             })
-//         // return new Promise(function(resolve, reject){resolve(msg);});
-//     }).catch(err=>{
-//         console.log(err);
-//         // return new Promise(function(resolve, reject){reject(err);});
-//     })
-// };
-//
-// router.post('/sendTextMessage',(req,res)=>{
-//     if (req.body.userId === undefined || req.body.textTitle === undefined || req.body.textContent === undefined) {
-//         res.send('格式错误');
-//         return;
-//     }
-//     if (req.body.userId === '' || req.body.textTitle === '' || req.body.textContent === '') {
-//         res.send('格式错误');
-//         return;
-//     }
-
-
-// {
-//     touser: messageData.userID,
-//         agentid: "210810582",
-//     msgtype: messageData.type,
-//     oa: {
-//     pc_message_url: message.messageUrl,
-//         message_url: message.messageUrl,
-//         head: {
-//         bgcolor: "FFBBBBBB",
-//             text: "账号出单"
-//     },
-//     body: {
-//         title: '出单：' + message.messageName + ' (' + message.messagePrincipal + ')',
-//             form: [{
-//             key: "订单编号：",
-//             value: message.messageOrderNumber
-//         }, {
-//             key: "SKU：",
-//             value: message.messageShopSku
-//         }, {
-//             key: "销售渠道：",
-//             value: message.messageSaleDitch
-//         }, {
-//             key: "金额：",
-//             value: message.messageShopPrice
-//         }],
-//             content: message.messageShopName,
-//             image: message.picUrl.replace(/SX55/, "SX500"),
-//             author: "Amazon"
-//     }
-// }
-// }
-
-
-//
-//     addSendTextMessageMission(messageData);
-//     // res.send('ok');
-// });
 
 module.exports = router;
