cd217a8582995fb4a16059cf2e7f1a9753938e0c
loyehu <2690264337@qq.com>
2019-07-19 11:23:18 +0800

    update

--- api/Rente.js
+++ api/Rente.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const DingTalk = require('node-dingtalk');
 const options = require('../public/config');
+const Group = require('../public/group');
 const dingtalk = new DingTalk(options);
 const Promise = require("bluebird");
 
@@ -65,15 +66,16 @@ router.post('/sendMessage',(req,res)=>{
     var messageData = {
         type: req.body.type,
         title: req.body.title,
-        userID: req.body.userID || 'manager1170|2156526054947871|215741075732367681|216039696838983459|116234674436480417|21565119341042799|220034443029640124|152263051426467241|2169332642841261|1333115857853755|1329376335850257|21573503281225898|094367066829863680',
     };
     if (req.body.type !== 'text') {
+        messageData.userID = Group.groupDisplay(JSON.parse(req.body.content).负责人);
         messageData.content = JSON.parse(req.body.content);
     } else {
         if (req.body.content.indexOf('等') !== -1) {
             // console.log(req.body.content + "等物流单号状态，不提示！！！");
             return;
         } else {
+            messageData.userID = Group.matchGroupDisplay(req.body.content);
             messageData.content = req.body.content;
         }
     }
